33f7b04a114291d29de81eccb83dc8194e25e673
Supriya Kempanna <supriya.kempanna@stratton.com.au>
2021-03-15 01:51:01 +1100

    commit update source files

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { PropTypes } from "prop-types";
 
-const Message = () => {
-    return (
-        <div>
-            <h3 className="text-center message"></h3>
-        </div>
-    )
-}
+import { INVALID_MESSAGE, VALID_MESSAGE } from "../utils/constants";
+
+const Message = ({ isValid }) => {
+  return (
+    <div>
+      <h3 className="text-center message">
+        {isValid ? VALID_MESSAGE : INVALID_MESSAGE}
+      </h3>
+    </div>
+  );
+};
+
+Message.propTypes = {
+  isValid: PropTypes.bool.isRequired,
+};
 
 export default Message;
